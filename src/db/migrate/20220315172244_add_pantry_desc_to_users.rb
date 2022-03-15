1ad8aff6bbe40731f8626d79555380e7899163a3
Payton Shaltis <shaltisp@gmail.com>
2022-03-15 14:24:41 -0400

    Adds new requests CRUD scaffold; can do all operations.
    The new CRUD scaffold is complete, and all of the basic operations
    associated with it can be done. There are currenlty no associations, but
    that will come soon.

--- src/db/migrate/20220315172244_add_pantry_desc_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPantryDescToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :pantry_desc, :string
-  end
-end
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_172244) do
+ActiveRecord::Schema.define(version: 2022_03_15_182039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2022_03_15_172244) do
   create_table "app_configs", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "name"
+    t.string "item_type"
+    t.string "description"
+    t.boolean "ispositive"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -33,7 +42,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_172244) do
     t.string "pantry_name"
     t.string "pantry_location"
     t.string "pantry_link"
-    t.string "pantry_desc"
+    t.text "pantry_desc"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
