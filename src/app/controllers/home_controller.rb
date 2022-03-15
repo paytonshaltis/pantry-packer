304c9402c702dac7c7cad39696181f281973613b
Payton Shaltis <shaltisp@gmail.com>
2022-03-15 11:22:48 -0400

    Adds new view for home controller: 'view_requests'.
    This page is the global view for seeing requests from all pantries.
    Other views will be generated with the scaffold.

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -6,17 +6,12 @@ class HomeController < ApplicationController
   # Overrides ApplicationController's instructions to check for login by default
   skip_before_action :authenticate_user!
 
-  # GET /
-  #
-  # This method is empty, yet it works?
-  # Rails controller methods can be completely empty and work just fine.
-  # It's the name of the method matching to a html.erb file that
-  # makes the link between the files.
-  # Find me @ app/views/home/index.html.erb
-  #
-  # Also, any local variable made inside this method will also exist
-  # in the view to be displayed. Do not look up information within the
-  # view itself. Do it here, please!
+  # Index page (home page)
   def index
   end
+
+  # Viewing requests page
+  def view_requests
+  end
+  
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -8,28 +8,9 @@
 
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # The root page, e.g. www.example.com/, is sent here
-  # root 'controller#method_in_controller'
+
   root 'home#index'
-  # Devise authentification pages. This controlls the user login
-  # and authentification system.
+  get 'home/view_requests'
   devise_for :users
 
-  # Examples:
-  #
-  # # Add app CRUD operations from a controller. Used with scaffolding.
-  # # CRUD -> create (API/POST), new (page/GET), index, show,
-  #           edit(page/GET), update(API/PATCH|PUT), destroy(API/DELETE)
-  # resources :my_controller
-  #
-  # # Add GET path for photos controller, index method
-  # get 'photos/index'
-  #
-  # # Resources, but only register these methods
-  # resources :photos, only: [:index, :new, :create, :destroy]
-  #
-  # # Add PUT path for the given url, but send it to a different controller
-  # # than rails would assume by the name. to: 'controller#method_name'.
-  # # It would have extected to: 'add_tags#add', which would fail
-  # put '/add_tags/add', to: 'users#add_tags'
 end
