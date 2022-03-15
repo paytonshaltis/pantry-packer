8ebe7cf9ce0844bd266a0503b3a22a5c6c95a2cf
Payton Shaltis <shaltisp@gmail.com>
2022-03-15 13:33:36 -0400

    Adds updating capabilities for a pantry's account.
    The new fields for the pantry account have been made editable through
    the standard devise account editing screen.

--- src/app/controllers/users/registrations_controller.rb
+++ src/app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -15,9 +15,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
   # def update
@@ -46,9 +46,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pantry_name, :pantry_location, :pantry_link, :pantry_desc])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
