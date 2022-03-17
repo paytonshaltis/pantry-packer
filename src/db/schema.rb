be00f8cbe47a26a32eebeb6af12349a56caf17e7
Payton Shaltis <shaltisp@gmail.com>
2022-03-17 17:28:46 -0400

    Fixes issue with bad migration.
    Removes a bad migration that prevented the server from starting up
    correctly.

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_191612) do
     t.string "pantry_name"
     t.string "pantry_location"
     t.string "pantry_link"
-    t.text "pantry_desc"
+    t.string "pantry_desc"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
