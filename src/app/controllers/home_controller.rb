b06bb75b4eb164c89095edf66212c899ffac8194
Payton Shaltis <shaltisp@gmail.com>
2022-04-18 22:28:59 -0400

    Implements general search bar for all user / request fields.
    The general search bar will now look through a combined total of 6
    attributes spanning across requests and even requests associated with
    pantries that match queries.

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -12,7 +12,8 @@ class HomeController < ApplicationController
 
   # Viewing requests page
   def search
-    @requests = Request.all
+    @requests = Request.all;
+    @requests_to_display = nil;
 
     # Store the parameters passed from the search field of the view.
     search_text = params[:search_text];
@@ -27,7 +28,48 @@ class HomeController < ApplicationController
     @positive_checked = params[:positive_id] ? true : false;
     @negative_checked = params[:negative_id] ? true : false;
 
-    # First, query requests by 
+    # Start by validating / cleansing input if the user entered a string.
+    # Downcase it and strip whitespace; replace semicolons to prevent injection.
+    query_text = '';
+    if search_text
+      query_text = search_text;
+      query_text = query_text.downcase();
+      query_text = query_text.strip();
+      query_text = query_text.gsub(/;/, '');
+    end
+
+    # Need to query for requests matching: 
+    # (1) request name            # (2) request description
+    request_matches = Request.where("lower(name) LIKE ?", "%#{query_text}%") | 
+                      Request.where("lower(description) LIKE ?", "%#{query_text}%");
+
+    # Need to query for pantries matching: 
+    # (1) pantry name             # (3) pantry description   
+    # (2) pantry location         # (4) pantry link
+    user_matches  = User.where("lower(pantry_name) LIKE ?", "%#{query_text}%") | 
+                    User.where("lower(pantry_location) LIKE ?", "%#{query_text}%") | 
+                    User.where("lower(pantry_link) LIKE ?", "%#{query_text}%") | 
+                    User.where("lower(pantry_desc) LIKE ?", "%#{query_text}%");
+
+    # From the matching users, store all requests associated with their IDs.
+    matching_ids = [];
+    user_matches.each { |user|
+      matching_ids.push(user.id);
+      puts user.id;
+    }
+
+    # Retrieve all requests with a matching user_id.
+    associated_requests = [];
+    matching_ids.each { |user_id|
+      Request.where("user_id=#{user_id}").each { |request| 
+        associated_requests.push(request);
+      } 
+    }
+
+    # Merge the request matches and requests from user matches.
+    @requests_to_display = (request_matches + associated_requests).uniq();
+
+    # Filter the requests to display by the checkboxes.
 
   end
   
